debd9d4b53394e51909647d175db63460553a343
hakuhaliun2002 <hakuhaliun2002@gmail.com>
2024-08-22 18:29:43 +0800

    Анхны апп хадгалж байна

---
+++ components/Player.tsx
@@ -0,0 +1,76 @@
+"use client"; // This is a client component 👈🏽
+import { useCallback, useEffect, useState } from "react";
+import videojs from "video.js";
+import "videojs-youtube";
+
+
+
+// const player = videojs('myvideo', {
+//   controls: true,
+//   autoplay: true,
+//   preload: 'auto',
+// });
+
+
+interface PlayerProps {
+  /**
+   *
+   */
+  techOrder: string[];
+  /**
+   * Is autoplay enabled for this video?
+   */
+  autoplay: boolean;
+  /**
+   * Should this video have controls?
+   */
+  controls: boolean;
+  /**
+   * A list of video sources.
+   */
+  sources: {
+    /**
+     * The source url.
+     */
+    src: string;
+    /**
+     * The type of source
+     */
+    type: string;
+  }[];
+}
+
+/**
+ * A simple video player component for displaying videos from external websites.
+ * @returns A Video.js video player element.
+ */
+const Player = (props: PlayerProps) => {
+  const [videoEl, setVideoEl] = useState<HTMLVideoElement | null>(null);
+  const onVideo = useCallback((el: HTMLVideoElement) => {
+    setVideoEl(el);
+  }, []);
+
+  useEffect(() => {
+    if (videoEl == null) {
+      return;
+    }
+
+    // our video.js player
+    const player = videojs(videoEl, props);
+
+    return () => {
+      player.dispose();
+    };
+  }, [props, videoEl]);
+
+  return (
+    <>
+      <h1>The implementation below is using react functions</h1>
+      <div data-vjs-player>
+        <video ref={onVideo} className="video-js" height={500} playsInline />
+      </div>
+    </>
+  );
+};
+
+export default Player;
